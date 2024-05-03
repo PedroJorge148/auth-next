1f4a91a5d1eed057cfe2638bcaa3b7b2cbc4c098
Pedro Jorge <pedrojorge2011@gmail.com>
2024-05-03 07:42:38 -0300

    refactor: change 2FA token expires time

--- src/data/tokens.ts
+++ src/data/tokens.ts
@@ -55,7 +55,7 @@ export async function generatePasswordResetToken(email: string) {
 export async function generateTwoFactorToken(email: string) {
   const token = randomInt(100_000, 1_000_000).toString()
   // TODO: Later change to 15 minutes
-  const expires = new Date(new Date().getTime() + 3600 * 1000)
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000) // 5min
 
   const existingToken = await getTwoFactorTokenByEmail(email)
 
